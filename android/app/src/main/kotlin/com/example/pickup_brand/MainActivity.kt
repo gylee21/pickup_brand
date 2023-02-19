1e1b36c84ac3c488d128bc9efe72168539103002
igayeon <mslgy0201@gmail.com>
2023-02-19 17:28:37 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/pickup_brand/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pickup_brand
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,101 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() async {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  static const String _title = 'Flutter Code Sample';
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: _title,
+      home: Splash(),
+    );
+  }
+}
+
+class Splash extends StatelessWidget {
+  const Splash({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+        splash: Image.asset("assets/logo.png"), nextScreen: const HomePage());
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 1;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+
+  WebViewController? _webViewController;
+  //final homeUrl = 'https://okmall.com/products/view?no=691275&app_test=1';
+  final homeUrl = 'https://ritzmall.com';
+
+  final List<Widget> _widgetOptions = <Widget>[
+    Text("MARKET"),
+    Text("POST"),
+    Text("MYPAGE"),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  void initState() {
+    _webViewController = WebViewController()
+      ..loadRequest(Uri.parse(homeUrl))
+      ..setJavaScriptMode(JavaScriptMode.unrestricted);
+    super.initState();
+  }
+
+  // 메인 위젯
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: WebViewWidget(controller: _webViewController!),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite_rounded),
+            label: '즐겨찾기',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '홈',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.people),
+            label: '마이페이지',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.lightGreen,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+}
